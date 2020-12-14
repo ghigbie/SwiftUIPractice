0deddc987081f10d1d77c47926666d4dad015e5e
ghigbie <gehigbie@gmail.com>
2020-12-13 22:58:25 -0500

    Initial commit

---
+++ SwiftUIPractice/SwiftUIPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIPracticeApp.swift
+//  SwiftUIPractice
+//
+//  Created by George Higbie on 12/13/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
